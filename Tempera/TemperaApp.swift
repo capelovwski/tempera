e8f97c417afc787cd4a46d5dc85d4a1cde07df0b
capelovwski <murilo.dafonseca@outlook.com>
2026-06-01 21:09:01 -0300

    Initial Commit

---
+++ Tempera/TemperaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperaApp.swift
+//  Tempera
+//
+//  Created by Murilo Capelo on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TemperaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
